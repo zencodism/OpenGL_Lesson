4aea5d2dcec0f7142fa88e368f4219527bc48c58
Marek Bogatek <marek.bogatek@gmail.com>
2015-02-05 17:19:43 +0100

    Renames, CocosTest fails to compile shader at this point

--- ShadedQuad.py
+++ ShadedQuad.py
@@ -1,9 +1,10 @@
-#! /usr/bin/env python
-# -*- coding: utf8 -*-
-
-import sys
 from ctypes import *
 from Image import *
+import sys
+ 
+import pygame
+from pygame.locals import *
+ 
 try:
     # For OpenGL-ctypes
     from OpenGL import platform
@@ -18,10 +19,11 @@ except ImportError:
         # finds the absolute path to the framework
         path = find_library('OpenGL')
         gl = cdll.LoadLibrary(path)
-
+ 
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
+ 
 glCreateShader = gl.glCreateShader
 glShaderSource = gl.glShaderSource
 glShaderSource.argtypes = [c_int, c_int, POINTER(c_char_p), POINTER(c_int)]
@@ -36,10 +38,7 @@ glAttachShader = gl.glAttachShader
 glLinkProgram = gl.glLinkProgram
 glGetError = gl.glGetError
 glUseProgram = gl.glUseProgram
-
-window = 0
-texture = 0
-
+ 
 GL_FRAGMENT_SHADER = 0x8B30
 GL_VERTEX_SHADER = 0x8B31
 GL_COMPILE_STATUS = 0x8B81
@@ -55,10 +54,10 @@ def compile_shader(source, shader_type):
     
     status = c_int()
     glGetShaderiv(shader, GL_COMPILE_STATUS, byref(status))
-#    if not status.value:
-#        print_log(shader)
-#        glDeleteShader(shader)
-#        raise ValueError, 'Shader compilation failed'
+    if not status.value:
+        print_log(shader)
+        glDeleteShader(shader)
+        raise ValueError, 'Shader compilation failed'
     return shader
  
 def compile_program(vertex_source, fragment_source):
@@ -89,7 +88,7 @@ def print_log(shader):
     if length.value > 0:
         log = create_string_buffer(length.value)
         glGetShaderInfoLog(shader, length, byref(length), log)
-        print log.value
+        print >> sys.stderr, log.value
 
 def LoadTextures():
     image = open("ground.jpg")
@@ -102,103 +101,59 @@ def LoadTextures():
     glTexImage2D(GL_TEXTURE_2D, 0, 3, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
     glPixelStorei(GL_UNPACK_ALIGNMENT,1)
     
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+    #glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+    
 
-program = compile_program('''
+if __name__ == '__main__':
+    glutInit(sys.argv)
+    pygame.init()
+    width, height = 800, 600
+    screen = pygame.display.set_mode((width, height), OPENGL | DOUBLEBUF)
+    program = compile_program('''
     // Vertex program
-    varying vec3 pos;
-    void main() {
-        pos = gl_Vertex.xyz;
+    void main(void)
+    {
+        gl_TexCoord[0] = gl_Vertex;
         gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
     }
     ''', '''
     // Fragment program
-    //varying vec3 pos;
+    varying vec2 v_texCoord;
+    uniform sampler2D s_texture;
     void main() {
-        gl_FragColor.rgb = pos.xyz;
+      //gl_FragColor = texture2D(s_texture, v_texCoord);
+      
+      gl_FragColor = texture2D(s_texture, gl_TexCoord[0]);
     }
     ''')
-
-def InitGL(Width, Height):
-    glEnable(GL_TEXTURE_2D)
-    glClearColor(0.0, 0.0, 0.0, 0.0)    # This Will Clear The Background Color To Black
-    glClearDepth(1.0)                    # Enables Clearing Of The Depth Buffer
-    glDepthFunc(GL_LESS)                # The Type Of Depth Test To Do
-    glEnable(GL_DEPTH_TEST)                # Enables Depth Testing
-    glShadeModel(GL_SMOOTH)                # Enables Smooth Color Shading
-    glEnable(GL_BLEND)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        
-    glUseProgram(program)
-    
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()                    # Reset The Projection Matrix
-                                        # Calculate The Aspect Ratio Of The Window
-    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
-    glMatrixMode(GL_MODELVIEW)
-    LoadTextures()
-
-# The function called when our window is resized (which shouldn't happen if you enable fullscreen, below)
-def ReSizeGLScene(Width, Height):
-    if Height == 0:                        # Prevent A Divide By Zero If The Window Is Too Small 
-        Height = 1
-
-    glViewport(0, 0, Width, Height)        # Reset The Current Viewport And Perspective Transformation
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+    gluPerspective(90.0, width/float(height), 1.0, 100.0)
     glMatrixMode(GL_MODELVIEW)
-
-# The main drawing function. 
-def DrawGLScene():
-    
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)    # Clear The Screen And The Depth Buffer
-    glLoadIdentity()                    # Reset The View
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_TEXTURE_2D)
+    LoadTextures()
+    glActiveTexture(GL_TEXTURE0)
+    quit = False
+    angle = 0
     glUseProgram(program)
-    glClearColor(0.3,0.3,0.3,1.0)            
-    glTranslatef(0.0,0.0,-4.0)
-    
-    glBegin(GL_QUADS)
-    
-    glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0)    # Bottom Left Of The Texture and Quad
-    glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0,  1.0)    # Bottom Right Of The Texture and Quad
-    glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0,  1.0)    # Top Right Of The Texture and Quad
-    glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0,  1.0)    # Top Left Of The Texture and Quad
-    
-    glEnd();
-    
-    glutSwapBuffers()
+        
+    while not quit:
+        for e in pygame.event.get():
+            if e.type in (QUIT, KEYDOWN):
+                quit = True
+        glLoadIdentity()
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glTranslatef(0.0,0.0,-4.0)
+        glRotate(angle, 0.0, 1.0, 0.0)
+        glBegin(GL_QUADS)
+        glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0)    # Bottom Left Of The Texture and Quad
+        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0,  1.0)    # Bottom Right Of The Texture and Quad
+        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0,  1.0)    # Top Right Of The Texture and Quad
+        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0,  1.0)    # Top Left Of The Texture and Quad
+        glEnd();
+        angle += 0.1 
+        pygame.display.flip()
 
-def keyPressed(*args):
-    sys.exit()
-
-def main():
-    global window
-    glutInit(sys.argv)
-
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
-    glutInitWindowSize(640, 480)
-    glutInitWindowPosition(0, 0)
-    
-    window = glutCreateWindow("Magic")
-
-    glutDisplayFunc(DrawGLScene)
-    glutIdleFunc(DrawGLScene)
-    glutReshapeFunc(ReSizeGLScene)
-    glutKeyboardFunc(keyPressed)
-
-    # Initialize our window. 
-    # glutFullScreen()
-    InitGL(640, 480)
-
-    glutMainLoop()
-
-if __name__ == "__main__":
-    main()
-       
